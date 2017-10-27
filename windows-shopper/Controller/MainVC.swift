441d6aebcd8705653e22d7bcb4b18567b7af2f04
gutNorbert <32197748+gutNorbert@users.noreply.github.com>
2017-10-27 12:26:08 +0200

    calculate btn

--- windows-shopper/Controller/MainVC.swift
+++ windows-shopper/Controller/MainVC.swift
@@ -9,10 +9,25 @@
 import UIKit
 
 class MainVC: UIViewController {
-
+    @IBOutlet weak var wageTxt: CurrencyTxtField!
+    @IBOutlet weak var priceTxt: CurrencyTxtField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        calcBtn.backgroundColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
+        calcBtn.setTitle("Calculate", for: .normal) // To set text on UIbutton always use .setTitle
+        calcBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calcBtn.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside) //obj-c
+        
+        wageTxt.inputAccessoryView = calcBtn
+        priceTxt.inputAccessoryView = calcBtn
+    }
+    
+    @objc func calculate(){
+        print("We got here")
+        
     }
 
 
